f808596e7f76e86f3e4666c4988c59ba9fdf887f
Yash Hirwani <hirwaniyash9@gmail.com>
2026-07-12 09:25:48 +0530

    Initialized folder structure

---
+++ Backend/TransitOp/src/main/java/com/yadg/TransitOp/TransitOpApplication.java
@@ -0,0 +1,13 @@
+package com.yadg.TransitOp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitOpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitOpApplication.class, args);
+	}
+
+}
---
+++ Backend/TransitOp/src/test/java/com/yadg/TransitOp/TransitOpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yadg.TransitOp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitOpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
